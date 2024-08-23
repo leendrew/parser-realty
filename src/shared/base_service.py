5ca5ac6e3eacbebc3adbf17e0dddb128c3d7c57f
leendrew <leendrew99@gmail.com>
2024-08-23 17:39:38 +0600

    chore: refactor

--- src/shared/base_service.py
+++ src/shared/base_service.py
@@ -2,8 +2,5 @@ from abc import ABC
 from src.shared import SessionDependency
 
 class BaseService(ABC):
-  def __init__(
-    self,
-    session: SessionDependency,
-  ) -> None:
+  def __init__(self, session: SessionDependency) -> None:
     self.session = session
