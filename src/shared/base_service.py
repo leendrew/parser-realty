9dbe408928a727154a1df60f00ed4e2e863fe224
leendrew <leendrew99@gmail.com>
2024-08-24 16:18:28 +0600

    fix: import

--- src/shared/base_service.py
+++ src/shared/base_service.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from src.shared import SessionDependency
+from .db_service import SessionDependency
 
 class BaseService(ABC):
   def __init__(self, session: SessionDependency) -> None:
