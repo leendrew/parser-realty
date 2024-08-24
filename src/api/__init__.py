0d01106070155af21a2b3ae9dc4497fe9504a302
leendrew <leendrew99@gmail.com>
2024-08-24 16:22:28 +0600

    feat: rewrite api routes

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
-from .parsing_results import parsing_result_router
+from .search_links.search_link_controller import router as search_link_router
+from .parsing_results.parsing_result_controller import router as parsing_result_router
 
 api_router = APIRouter(
   prefix="/api",
 )
 
+routers = [
+  search_link_router,
+  parsing_result_router,
+]
+
+for router in routers:
+  api_router.include_router(router)
