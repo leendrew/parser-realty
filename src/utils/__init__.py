6caee796fb9b50f11d806c2df556160872488cbc
leendrew <leendrew99@gmail.com>
2024-08-25 13:59:41 +0600

    feat: link validator

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,2 @@
 from .case_converter import CaseConverter
+from .link_validator import LinkValidator
---
+++ src/utils/link_validator.py
@@ -0,0 +1,22 @@
+import re
+from src.api.search_links.search_link_types import SourceName
+
+class LinkValidator:
+  @staticmethod
+  def is_valid_https(link: str) -> bool:
+    return link.startswith("https://")
+
+  @staticmethod
+  def is_valid_source(source: SourceName, link: str) -> bool:
+    https_part_regex = "https:\/\/"
+    subdomain_part_regex = ".*\."
+    domain_part_regex = "\.[^\/]*"
+    regex_link_by_source = {
+      SourceName.avito: re.compile(pattern=rf"^{https_part_regex}www\.avito{domain_part_regex}"),
+      SourceName.yandex: re.compile(rf"^{https_part_regex}\.realty\.ya{domain_part_regex}"),
+      SourceName.cian: re.compile(rf"^{https_part_regex}{subdomain_part_regex}cian{domain_part_regex}"),
+      SourceName.domclick: re.compile(rf"^{https_part_regex}{subdomain_part_regex}domclick{domain_part_regex}"),
+    }
+    regex = regex_link_by_source[source]
+
+    return bool(re.match(pattern=regex, string=link))
