262d3e7a627977b7eeaf3b3b42f3785657d5f0bc
leendrew <leendrew99@gmail.com>
2024-08-25 15:08:23 +0600

    feat: search link dtos

---
+++ src/api/search_links/search_link_dtos.py
@@ -0,0 +1,39 @@
+from uuid import UUID
+from typing import Annotated
+from pydantic import (
+  BaseModel,
+  model_validator,
+)
+from fastapi import (
+  Query,
+  Depends,
+)
+from src.utils import LinkValidator
+from .search_link_types import SourceName
+
+class CreateOnePayloadDto(BaseModel):
+  link: str
+  source_name: SourceName
+  user_id: UUID
+
+  @model_validator(mode="after")
+  def validate_link(self):
+    is_link_https = LinkValidator.is_valid_https(self.link)
+    if not is_link_https:
+      raise ValueError("Невалидный протокол")
+
+    is_valid_source_link = LinkValidator.is_valid_source(
+      source=self.source_name,
+      link=self.link
+    )
+    if not is_valid_source_link:
+      raise ValueError("Ссылки данного сайта не поддерживаются")
+
+    return self
+
+class GetAllByQueryDto(BaseModel):
+  id: Annotated[int | None, Query(default=None)] = None
+  source_name: Annotated[SourceName | None, Query()] = None
+  user_id: Annotated[UUID | None, Query()] = None
+
+GetAllByQueryDtoDependency = Annotated[GetAllByQueryDto, Depends()]
