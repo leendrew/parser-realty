b4c93b80bb6226d2557709769776201f3b67e05f
leendrew <leendrew99@gmail.com>
2024-09-22 10:53:41 +0600

    feat: extend user service

--- src/api/users/user_service.py
+++ src/api/users/user_service.py
@@ -7,7 +7,10 @@ from fastapi import (
   Depends,
   HTTPException,
 )
-from sqlalchemy import select
+from sqlalchemy import (
+  select,
+  delete,
+)
 from src.shared import (
   BaseService,
   Logger,
@@ -17,6 +20,25 @@ from src.models.user_model import UserModel
 logger = Logger().get_instance()
 
 class UserService(BaseService):
+  async def create_one(self) -> UserModel:
+    model = UserModel()
+    try:
+      await self.session.add(model)
+      await self.session.commit()
+      await self.session.refresh()
+
+      return model
+
+    except Exception:
+      await self.session.rollback()
+
+      logger.exception(f"Ошибка при создании пользователя")
+      # TODO: correct status code
+      raise HTTPException(
+        status_code=400,
+        detail="Ошибка при создании пользователя",
+      )
+
   async def get_all(self) -> Sequence[UserModel]:
     stmt = select(UserModel)
     users = await self.session.scalars(stmt)
@@ -25,17 +47,16 @@ class UserService(BaseService):
 
   async def get_one(
     self,
-    user_id: UUID,
+    id: UUID,
   ) -> UserModel:
     stmt = (
       select(UserModel)
-      .where(UserModel.id == user_id)
+      .where(UserModel.id == id)
     )
 
     user = await self.session.scalar(stmt)
     if not user:
-      logger.error(f"Пользователь с id \"{id}\" отсутствует")
-
+      logger.error(f"Пользователь с id \"{id}\" не найден")
       # TODO: correct status code
       raise HTTPException(
         status_code=400,
@@ -43,5 +64,30 @@ class UserService(BaseService):
       )
 
     return user
+  
+  async def delete_one(
+    self,
+    id: UUID,
+  ) -> UserModel:
+    stmt = (
+      delete(UserModel)
+      .where(UserModel.id == id)
+      .returning(UserModel)
+    )
+
+    try:
+      model = await self.session.scalar(stmt)
+
+      return model
+
+    except Exception:
+      await self.session.rollback()
+
+      logger.exception(f"Ошибка при удалении пользователя с id \"{id}\"")
+      # TODO: correct status code
+      raise HTTPException(
+        status_code=400,
+        detail="Ошибка при удалении пользователя",
+      )
 
 UserServiceDependency = Annotated[UserService, Depends()]
