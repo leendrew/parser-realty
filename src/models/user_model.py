91fe7fa892a2c3e3ca00a6af974a2fe833643db2
leendrew <leendrew99@gmail.com>
2024-09-24 23:12:23 +0600

    feat: new models

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -10,6 +10,7 @@ from .base_model import BaseModel
 if TYPE_CHECKING:
   from .search_link_model import SearchLinkModel
   from .user_search_link_model import UserSearchLinkModel
+  from .user_telegram_model import UserTelegramModel
 
 class UserModel(BaseModel):
   __tablename__ = "users"
@@ -28,4 +29,8 @@ class UserModel(BaseModel):
   user_users_search_links_associations: Mapped[list["UserSearchLinkModel"]] = relationship(
     back_populates="user",
     viewonly=True,
+  )
+  # o2o
+  telegram: Mapped["UserTelegramModel"] = relationship(
+    back_populates="user",
   )
---
+++ src/models/user_telegram_model.py
@@ -0,0 +1,35 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+from sqlalchemy.orm import (
+  mapped_column,
+  relationship,
+  Mapped,
+)
+from sqlalchemy import (
+  BigInteger,
+  UUID as PSQL_UUID,
+  ForeignKey,
+)
+from .base_model import BaseModel
+if TYPE_CHECKING:
+  from .user_model import UserModel
+
+class UserTelegramModel(BaseModel):
+  __tablename__ = "users_telegrams"
+
+  telegram_id: Mapped[int] = mapped_column(
+    BigInteger,
+    primary_key=True,
+  )
+
+  user_id: Mapped[UUID] = mapped_column(
+    PSQL_UUID,
+    ForeignKey(
+      "users.id",
+      ondelete="CASCADE",
+    ),
+  )
+  # o2o
+  user: Mapped["UserModel"] = relationship(
+    back_populates="telegram",
+  )
