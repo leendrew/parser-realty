85f6349cc8f04ad2bfcc65ee33801cbe6106f250
leendrew <leendrew99@gmail.com>
2024-09-27 09:31:03 +0600

    feat: parsing result types

--- src/api/parsing_results/parsing_result_types.py
+++ src/api/parsing_results/parsing_result_types.py
@@ -1,6 +1,31 @@
+from typing import (
+  Optional,
+  TYPE_CHECKING,
+)
 from enum import Enum
+from pydantic import BaseModel
+if TYPE_CHECKING:
+  from src.models.user_search_link_model import UserSearchLinkModel
+  from src.models.city_metro_station_model import CityMetroStationModel
 
 class HousingType(Enum):
   room = "room"
   flat = "flat"
   house = "house"
+
+class ParsingResultBase(BaseModel):
+  direct_link: str
+  housing_type: HousingType
+  flat_room_type: int
+  floor: str
+  flat_area: str
+  price: int
+  commission_percent: int | None
+  deposit_percent: int | None
+
+class ParsingResult(ParsingResultBase):
+  metro_station_name: str | None
+
+class CreateOnePayload(ParsingResultBase):
+  user_search_link: "UserSearchLinkModel"
+  city_metro_station: Optional["CityMetroStationModel"]
