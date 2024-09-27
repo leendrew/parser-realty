3f9f23c4ea7f32a3499f85aba88205cb87ed4a49
leendrew <leendrew99@gmail.com>
2024-09-27 09:05:05 +0600

    feat: tg bot middleware

---
+++ src/tg_bot/middlewares/__init__.py
@@ -0,0 +1,1 @@
+from .services_middleware import ServicesMiddleware
---
+++ src/tg_bot/middlewares/services_middleware.py
@@ -0,0 +1,33 @@
+from typing import (
+  Callable,
+  Dict,
+  Any,
+  Awaitable,
+)
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from src.shared import db_service
+from src.api.users_telegrams.user_telegram_service import UserTelegramService
+from src.api.users.user_service import UserService
+from src.api.search_links.search_link_service import SearchLinkService
+from src.api.parsing_results.parsing_result_service import ParsingResultService
+
+class ServicesMiddleware(BaseMiddleware):
+  async def __call__(
+    self,
+    handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+    event: Message,
+    data: Dict[str, Any],
+  ) -> Any:
+    async with db_service.session_factory() as session:
+      user_telegram_service = UserTelegramService(session=session)
+      user_service = UserService(session=session)
+      search_link_service = SearchLinkService(session=session)
+      parsing_result_service = ParsingResultService(session=session)
+
+      data["user_telegram_service"] = user_telegram_service
+      data["user_service"] = user_service
+      data["search_link_service"] = search_link_service
+      data["parsing_result_service"] = parsing_result_service
+
+      return await handler(event, data)
