b66fd62c5e85589b0dda8536085ba016d4b0f702
leendrew <leendrew99@gmail.com>
2024-09-29 16:43:29 +0600

    feat: source name title

--- src/api/search_links/search_link_types.py
+++ src/api/search_links/search_link_types.py
@@ -4,3 +4,9 @@ class SourceName(Enum):
   avito = "avito"
   yandex = "yandex"
   cian = "cian"
+
+source_name_title_map = {
+  SourceName.avito: "Авито",
+  SourceName.yandex: "Яндекс Недвижимость",
+  SourceName.cian: "Циан",
+}
