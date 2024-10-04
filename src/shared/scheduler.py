30f76e8276d009ab7710f6f0e7ab6542012b5932
leendrew <leendrew99@gmail.com>
2024-10-04 22:37:39 +0600

    feat: scheduler jobs

--- src/shared/scheduler.py
+++ src/shared/scheduler.py
@@ -1,9 +1,37 @@
+from asyncio import create_task
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from src.parsing.parsing_service import ParsingService
+from src.api.search_links.search_link_types import SearchType
 from .logger import Logger
 
 logger = Logger().get_instance()
 
 scheduler = AsyncIOScheduler()
+
+cron_interval_by_search_type_map = {
+  SearchType.rent: {
+    "minute": "*/30",
+  },
+  SearchType.purchase: {
+    "minute": "0",
+    "hour": "3",
+  },
+}
+
+async def parse_rent_job() -> None:
+  parsing_service = ParsingService()
+  print("start job rent")
+  create_task(parsing_service.dispatch(SearchType.rent))
+
+async def parse_purchase_job() -> None:
+  parsing_service = ParsingService()
+  print("start job purchase")
+  create_task(parsing_service.dispatch(SearchType.purchase))
+
+scheduler.add_job(
+  parse_rent_job,
+  "cron",
+  minute="*/1",
+)
 
 # scheduler.print_jobs()
