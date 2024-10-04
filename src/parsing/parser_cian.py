2d483a9c3ddf0d7d6cee37c2cc6fd98d97c5b27b
leendrew <leendrew99@gmail.com>
2024-10-04 22:34:30 +0600

    chore: refactor

--- src/parsing/parser_cian.py
+++ src/parsing/parser_cian.py
@@ -6,9 +6,7 @@ from bs4 import (
 )
 from src.shared import Logger
 from .parser_base import ParserBase
-from src.api.parsing_results.parsing_result_types import (
-  ParsingResult,
-)
+from src.api.parsing_results.parsing_result_types import ParsingResult
 from src.api.search_links.search_link_types import SourceName
 
 logger = Logger().get_instance()
@@ -21,13 +19,11 @@ class ParserCian(ParserBase):
     soup = self.parser.with_lxml(markup=markup)
     body = soup.find(name="body")
 
-    container_regex = re.compile(r"cards-wrapper")
-    item_regex = re.compile(r"card-wrapper")
-    flat_area_regex = re.compile(r"(\d+)")
-    floor_regex = re.compile(r"([0-9/]+)")
+    card_container_regex = re.compile(r"card__content")
     price_regex = re.compile(r"\d+")
 
-    container = body.find(class_=container_regex)
+    # [search-entry-frontend][master][be0ecda7]
+    container = body.find(attrs={"data-name": "ListingLayout"})
     if not container:
       message = f"Не нашел контейнер при парсинге источника \"{SourceName.cian.value}\""
       logger.error(message)
@@ -35,30 +31,15 @@ class ParserCian(ParserBase):
 
     result: list[ParsingResult] = []
 
-    items: ResultSet[Tag | NavigableString] | None = container.find_all(class_=item_regex)
+    items: ResultSet[Tag | NavigableString] | None = container.find_all(attrs={"data-name": "CardContainer"})
     for item in items:
       try:
-        item_link = item.find(name="a")
+        item_content_container = item.find(class_=card_container_regex)
+
+        item_link = item_content_container.find(name="a")
         direct_link = item_link.get("href")
 
-        item_title_text = item_link.text.strip()
-        arr = item_title_text.split("･")
-        flat_area_raw = arr[1]
-        floor_raw = arr[2]
-
-        flat_area_match = re.search(pattern=flat_area_regex, string=flat_area_raw)
-        flat_area = flat_area_match.group(1)
-
-        floor_match = re.search(pattern=floor_regex, string=floor_raw)
-        floor = floor_match.group(1)
-
-        metro_station_name = None
-        item_metro_station_root_container = item.find(attrs={"data-name": "SpecialGeo"})
-        item_metro_station_name_container = item_metro_station_root_container.find(name="a")
-        if item_metro_station_name_container:
-          metro_station_name = item_metro_station_name_container.text
-
-        item_price_container = item.find(attrs={"data-name": "CardPriceBlock"})
+        item_price_container = item_content_container.find(attrs={"data-mark": "PriceLayout"})
         price_raw = item_price_container.text
         price_matches = re.findall(pattern=price_regex, string=price_raw)
         price = int("".join(price_matches))
@@ -68,12 +49,9 @@ class ParserCian(ParserBase):
 
         parsing_result = ParsingResult(
           direct_link=direct_link,
-          floor=floor,
-          flat_area=flat_area,
           price=price,
           commission_percent=commission_percent,
           deposit_percent=deposit_percent,
-          metro_station_name=metro_station_name,
         )
         result.append(parsing_result)
 
