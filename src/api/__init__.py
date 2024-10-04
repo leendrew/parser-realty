13adcfedbc3128f73a32a1357603cb801a231933
leendrew <leendrew99@gmail.com>
2024-10-04 22:41:58 +0600

    chore: comment routers

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
-from .search_links.search_link_controller import router as search_link_router
-from .parsing_results.parsing_result_controller import router as parsing_result_router
+# from .search_links.search_link_controller import router as search_link_router
+# from .parsing_results.parsing_result_controller import router as parsing_result_router
 
 api_router = APIRouter(
   prefix="/api",
 )
 
 routers = [
-  search_link_router,
-  parsing_result_router,
+  # search_link_router,
+  # parsing_result_router,
 ]
 
 for router in routers:
