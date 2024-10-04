a1c22fb4981ef5c974dedcaaa1cff924de532747
leendrew <leendrew99@gmail.com>
2024-10-04 22:34:49 +0600

    chore: refactor

--- src/api/parsing_results/parsing_result_controller.py
+++ src/api/parsing_results/parsing_result_controller.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 from src.shared import Logger
-from .parsing_result_service import ParsingResultServiceDependency
-from ..search_links.search_link_service import SearchLinkServiceDependency
-from ..search_links.search_link_types import SourceName
-from ..users_search_links.user_search_link_service import UserSearchLinkServiceDependency
-from src.parsing.parsing_service import ParsingServiceDependency
+# from .parsing_result_service import ParsingResultServiceDependency
+# from ..search_links.search_link_service import SearchLinkServiceDependency
+# from ..search_links.search_link_types import SourceName
+# from ..users_search_links.user_search_link_service import UserSearchLinkServiceDependency
+# from src.parsing.parsing_service import ParsingServiceDependency
 
 logger = Logger().get_instance()
 
-from asyncio import create_task, gather
+# from asyncio import create_task, gather
 
 router = APIRouter(
   # prefix="/parsing-results",
@@ -16,18 +16,18 @@ router = APIRouter(
   prefix="/test",
 )
 
-@router.get("/")
-async def get_parsing_results(
-  parsing_service: ParsingServiceDependency,
-  parsing_result_service: ParsingResultServiceDependency,
-  search_link_service: SearchLinkServiceDependency,
-  user_search_link_service: UserSearchLinkServiceDependency,
-):
+# @router.get("/")
+# async def get_parsing_results(
+#   parsing_service: ParsingServiceDependency,
+#   parsing_result_service: ParsingResultServiceDependency,
+#   search_link_service: SearchLinkServiceDependency,
+#   user_search_link_service: UserSearchLinkServiceDependency,
+# ):
   # UserSearchLinkModel with relationships
   # CityMetroStationModel
 
   # user_id = "0c44ed8c-2fd0-439f-9e73-84b5776c34ac"
-  user_id = "ab7536cd-9b3a-4835-a315-aa523c91a0f4"
+  # user_id = "ab7536cd-9b3a-4835-a315-aa523c91a0f4"
   
   # res = await parsing_service.parse_test(source=SourceName.avito)
   # return res
@@ -45,8 +45,8 @@ async def get_parsing_results(
   # logger.info(f"Создаю задачи")
   # all = []
   # for link in links:
-  #   task = parsing_service.dispatcher(source=link.source_name_enum, link=link.search_link)
-  #   task2 = parsing_service.dispatcher(source=link.source_name_enum, link=link.search_link)
+  #   task = parsing_service.dispatch()
+  #   task2 = parsing_service.dispatch()
   #   all.append(task)
   #   all.append(task2)
   # res = await gather(*all)
