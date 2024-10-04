97c7f06a1641306ce63b6fd09e78c0c8f0514b97
leendrew <leendrew99@gmail.com>
2024-10-04 22:36:16 +0600

    feat: new user search link service methods

--- src/api/users_search_links/user_search_link_service.py
+++ src/api/users_search_links/user_search_link_service.py
@@ -14,25 +14,48 @@ from src.shared import (
 )
 from src.models.user_search_link_model import UserSearchLinkModel
 from src.models.search_link_model import SearchLinkModel
+from src.api.search_links.search_link_types import SearchType
 
 logger = Logger().get_instance()
 
 class UserSearchLinkService(BaseService):
+  async def get_one_by(
+    self,
+    id: int | None = None,
+  ) -> UserSearchLinkModel | None:
+    stmt = (
+      select(UserSearchLinkModel)
+      # .join(UserSearchLinkModel.user)
+      # .options(joinedload(UserSearchLinkModel.user))
+      .join(UserSearchLinkModel.search_link)
+      .options(joinedload(UserSearchLinkModel.search_link))
+    )
+
+    filters = []
+    if id is not None:
+      filters.append(UserSearchLinkModel.id == id)
+
+    if filters:
+      stmt = stmt.filter(*filters)
+
+    result = await self.session.scalar(stmt)
+
+    return result
+
   async def get_all_by(
     self,
     id: int | None = None,
     user_id: UUID | None = None,
     search_link_id: int | None = None,
+    search_link_search_type: SearchType | None = None,
     is_link_active: bool | None = None,
   ) -> Sequence[UserSearchLinkModel]:
     stmt = (
       select(UserSearchLinkModel)
       .join(UserSearchLinkModel.user)
-      # add relation model to result
-      # .options(joinedload(UserSearchLinkModel.user))
+      .options(joinedload(UserSearchLinkModel.user))
       .join(UserSearchLinkModel.search_link)
-      # add relation model to result
-      # .options(joinedload(UserSearchLinkModel.search_link))
+      .options(joinedload(UserSearchLinkModel.search_link))
     )
 
     filters = []
@@ -42,6 +65,8 @@ class UserSearchLinkService(BaseService):
       filters.append(UserSearchLinkModel.user_id == user_id)
     if search_link_id is not None:
       filters.append(UserSearchLinkModel.search_link_id == search_link_id)
+    if search_link_search_type is not None:
+      filters.append(SearchLinkModel.search_type == search_link_search_type.value)
     if is_link_active is not None:
       filters.append(SearchLinkModel.is_active == is_link_active)
 
