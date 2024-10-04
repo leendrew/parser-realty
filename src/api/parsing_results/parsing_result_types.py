b3571a838737df062e46f2a545088ec8f00e05ca
leendrew <leendrew99@gmail.com>
2024-10-04 22:35:40 +0600

    chore: refactor

--- src/api/parsing_results/parsing_result_types.py
+++ src/api/parsing_results/parsing_result_types.py
@@ -1,23 +1,7 @@
-from typing import (
-  TYPE_CHECKING,
-  Optional,
-)
 from pydantic import BaseModel
-if TYPE_CHECKING:
-  from src.models.user_search_link_model import UserSearchLinkModel
-  from src.models.city_metro_station_model import CityMetroStationModel
 
-class ParsingResultBase(BaseModel):
+class ParsingResult(BaseModel):
   direct_link: str
-  floor: str
-  flat_area: str
   price: int
   commission_percent: int | None
   deposit_percent: int | None
-
-class ParsingResult(ParsingResultBase):
-  metro_station_name: str | None
-
-class CreateOnePayload(ParsingResultBase):
-  user_search_link: "UserSearchLinkModel"
-  city_metro_station: Optional["CityMetroStationModel"]
