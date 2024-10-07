815c76e42befbe341a7b80454fb368fee2e6b0d8
leendrew <leendrew99@gmail.com>
2024-10-07 23:04:33 +0600

    chore: refactor

--- src/tg_bot/main.py
+++ src/tg_bot/main.py
@@ -1,7 +1,13 @@
-from asyncio import run
+from asyncio import run, create_task
+from src.shared import (
+  queues,
+  scheduler,
+)
 from .bot import tg_bot
 
-async def main():
+async def main() -> None:
+  create_task(queues.start())
+  scheduler.start()
   await tg_bot.start()
 
 if __name__ == "__main__":
